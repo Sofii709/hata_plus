968779daf376b049227be33c6b7090f05aa78f78
kava_f <sofiasmile2011@gmail.com>
2026-02-08 15:05:45 +0300

    doing forms

--- js/script.js
+++ js/script.js
@@ -17,7 +17,6 @@ black_bg.onclick = () => {
 function openMenu(id) {
   id.style.display = 'block'
   id.style.position = 'fixed'
-  id.style.top = '150rem'
   id.style.left = '0'
   id.style.width = 'auto'
   id.style.zIndex = '1'
